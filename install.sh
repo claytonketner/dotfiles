416d7b99ad8ae860f47217accdea93ac8021be49
claytonketner <claytonketner@me.com>
2015-05-02 09:43:08 -0700

    Basic installation of vim packages - could be fancier

--- install.sh
+++ install.sh
@@ -18,3 +18,13 @@ do
         ln -s $CURRENT_DIR/$FILENAME ~/$FILENAME
     fi
 done
+
+echo "Installing vim plugins..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+cd ~/.vim/bundle
+git clone git://github.com/tpope/vim-fugitive.git
+vim -u NONE -c "helptags vim-fugitive/doc" -c q
+git clone git://github.com/tpope/vim-surround.git
+git clone git://github.com/tpope/vim-repeat.git
